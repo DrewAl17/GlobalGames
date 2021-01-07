7d5939da2f0ad82996ea76e330fce99ab1237204
Andre Lajas <Andre.Franco.Lajas@formandos.cinel.pt>
2021-01-07 19:12:43 +0000

    Criar a base de dados e tabela para serviços

---
+++ GlobalGamesCET50/Dadoss/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using GlobalGamesCET50.Dadoss.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalGamesCET50.Dadoss
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Servicos> Servicos { get; set; }
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ GlobalGamesCET50/Dadoss/Entidades/Servicos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalGamesCET50.Dadoss.Entidades
+{
+    public class Servicos
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Mensagens { get; set; }
+
+        
+    }
+}
